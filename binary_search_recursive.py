243bd55f1fba0abbcc1503058962e7f6db99b49f
Collins Nixon Tech <cnixontech@gmail.com>
2022-07-11 07:35:01 +0300

    testing the algorithm

--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -1,7 +1,7 @@
-arr_one = [1,6,9,13,24,36,40]
+arr_one = [1, 6, 9, 13, 24, 36, 40]
 
 
-def binary_search_two(arr,leftmost,rightmost,target):
+def binary_search_two(arr, leftmost, rightmost, target):
 
     if rightmost >= leftmost:
         mid = (rightmost + leftmost) // 2
@@ -9,11 +9,11 @@ def binary_search_two(arr,leftmost,rightmost,target):
             return "number " + str(arr[mid]) + " found"
         else:
             if target > arr[mid]:
-                return binary_search_two(arr,leftmost,mid+1,target)
+                return binary_search_two(arr, leftmost, mid+1, target)
             else:
-                return binary_search_two(arr,rightmost,mid - 1,target)
+                return binary_search_two(arr, rightmost, mid - 1, target)
     else:
         return " not found, please try again"
 
 
-print(binary_search_two(arr_one,0,len(arr_one)-1,13))
+print(binary_search_two(arr_one, 0, len(arr_one)-1, 13))
