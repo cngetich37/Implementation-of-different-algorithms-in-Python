c6f6f44e177dc3becedeb76a54e353bf429004c5
Collins Nixon Tech <cnixontech@gmail.com>
2022-07-17 12:07:35 +0300

    updated the comments

--- bubble_sort_accept_inputs_from_the_user.py
+++ bubble_sort_accept_inputs_from_the_user.py
@@ -8,7 +8,8 @@ print("add other numbers")
 #Iterate through the entered integers as you add to the list
 for k in range(entered_numbers):
     my_list.append(int(input()))
-
+    
+#Bubble sort algorithm
 for j in range(len(my_list)-1,0,-1):
     for i in range(j):
         if my_list[i] > my_list[i+1]:
