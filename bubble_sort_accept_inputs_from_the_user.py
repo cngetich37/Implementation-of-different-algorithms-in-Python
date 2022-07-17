ec7076179857634e976bfd0f6de4787361e1db9f
Collins Nixon Tech <cnixontech@gmail.com>
2022-07-17 12:14:20 +0300

    added more comments

--- bubble_sort_accept_inputs_from_the_user.py
+++ bubble_sort_accept_inputs_from_the_user.py
@@ -1,21 +1,18 @@
-#begin with an empty list
+# begin with an empty list
 my_list = []
-#Ask users to enter the numbers
-entered_numbers = int(input("Please enter the number:"))
+# entered numbers are the number of integers that should be entered
+entered_numbers = int(input("Please the number of integers:"))
 
-#add other numbers that you were to sort with the entered number
-print("add other numbers")
-#Iterate through the entered integers as you add to the list
+# add other numbers that you were to sort with the entered number
+print("enter the values:")
+# Iterate through the entered integers as you add to the list
 for k in range(entered_numbers):
     my_list.append(int(input()))
-    
-#Bubble sort algorithm
-for j in range(len(my_list)-1,0,-1):
+
+# Bubble sort algorithm
+for j in range(len(my_list)-1, 0, -1):
     for i in range(j):
         if my_list[i] > my_list[i+1]:
             my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
 
 print("Sorted list:"+str(my_list))
-
-
-
