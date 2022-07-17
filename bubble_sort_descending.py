ad987c59851bbc6910cd893b143468291d583116
Collins Nixon Tech <cnixontech@gmail.com>
2022-07-17 11:09:54 +0300

    added bubble sort both ascending and descending order

---
+++ bubble_sort_descending.py
@@ -0,0 +1,13 @@
+# unsorted list
+my_list_one = [10, 15, 4, 23, 0]
+
+# iterate through the list and compare each element with an adjacent element using FOR Loop
+for j in range(len(my_list_one)-1):
+    for i in range(len(my_list_one)-1):
+    # compare the first element with an adjacent element
+        if my_list_one[i] < my_list_one[i+1]:
+        # if the adjancent element is greater than the former element, you swap
+            my_list_one[i], my_list_one[i+1] = my_list_one[i+1], my_list_one[i]
+
+#this will print the list in the ascending order from the least to the greatest
+print(my_list_one)
